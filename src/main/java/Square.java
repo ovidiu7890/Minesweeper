c3ad7ebc92c95504975fc3bf14230cc6d5350840
ovidiu7890 <ovidiulates38@gmail.com>
2025-02-27 20:05:17 +0200

    Second xommit

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Square {
+    JButton button;
+    private int numberOfBombs;
+    private boolean bomb;
+    public Square(boolean bomb) {
+        button = new JButton();
+        this.numberOfBombs=0;
+        this.bomb=bomb;
+    }
+
+    public void setBomb(boolean bomb) {
+        this.bomb = bomb;
+    }
+
+    public void setNumberOfBombs(int numberOfBombs) {
+        this.numberOfBombs = numberOfBombs;
+    }
+
+    public boolean isBomb() {
+        return bomb;
+    }
+
+    public int getNumberOfBombs() {
+        return numberOfBombs;
+    }
 }
